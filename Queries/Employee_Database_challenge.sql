f8b037ec1ea879295760769235702f9b47acdfcd
Kyle Kramer <kyle@Kyles-MacBook-Air.local>
2022-02-07 19:47:36 -0600

    Deliverable 2 Upload

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -30,3 +30,22 @@ SELECT 	COUNT (ut.title),
 	FROM unique_tables as ut
 	GROUP BY ut.title
 	ORDER BY ut.count DESC;
+
+-- Mentorship Eligibility Table
+SELECT	DISTINCT ON (e.emp_no)
+		e.emp_no,
+		e.first_name,
+		e.last_name,
+		e.birth_date,
+		de.from_date,
+		de.to_date,
+		ti.title
+	INTO mentorship_eligibility
+	FROM employees as e
+	INNER JOIN dept_employees as de
+		ON (de.emp_no = e.emp_no)
+	INNER JOIN titles as ti
+		ON (ti.emp_no = e.emp_no)
+	WHERE ti.to_date = '9999-01-01'
+		AND e.birth_date BETWEEN '1965-01-01' AND '1965-12-31'
+	ORDER BY e.emp_no ASC;
